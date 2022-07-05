797ede11f605aebae9018a41b0997f30794ebd3d
Noah Elkhatali <noahpc@live.dk>
2022-07-05 02:57:24 +0200

    new keymaps are silent by default

--- nvim/.config/nvim/lua/user/keys.lua
+++ nvim/.config/nvim/lua/user/keys.lua
@@ -1,4 +1,13 @@
-local kset = vim.keymap.set
+-- Sets keymap with default :map-arguments (custom opts override defaults)
+local function kset(mode, lhs, rhs, opts)
+    local default_opts = { silent = true }
+    if opts then
+        opts = vim.tbl_extend('force', default_opts, opts)
+    else
+        opts = default_opts
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
 
 -- Leader key
 vim.g.mapleader = ','
@@ -14,43 +23,43 @@ kset('n', '<C-l>', '<C-w>l')
 kset('t', '<ESC>', '<C-\\><C-n>') -- escape terminal
 
 -- Resize splits
-kset('n', '<C-Up>', ':resize -2<CR>')
-kset('n', '<C-Down>', ':resize +2<CR>')
-kset('n', '<C-Left>', ':vertical resize -2<CR>')
-kset('n', '<C-Right>', ':vertical resize +2<CR>')
+kset('n', '<C-Up>', '<cmd>resize -2<CR>')
+kset('n', '<C-Down>', '<cmd>resize +2<CR>')
+kset('n', '<C-Left>', '<cmd>vertical resize -2<CR>')
+kset('n', '<C-Right>', '<cmd>vertical resize +2<CR>')
 
 -- Navigate buffers
-kset('n', '<A-h>', ':bprevious<CR>')
-kset('n', '<A-l>', ':bnext<CR>')
+kset('n', '<A-h>', '<cmd>bprevious<CR>')
+kset('n', '<A-l>', '<cmd>bnext<CR>')
 
 -- Move text (some of these don't work)
--- kset('n', '<A-j>', ':move .+1<CR>==gi<ESC>')
--- kset('n', '<A-k>', ':move .-2<CR>==gi<ESC>')
-kset('v', '<A-j>', ':move .+1<CR>==')
-kset('v', '<A-k>', ':move .-2<CR>==')
--- kset('x', '<A-j>', ":move '>+1<CR>gv-gv")
--- kset('x', '<A-k>', ":move '<-2<CR>gv-gv")
+-- kset('n', '<A-j>', '<cmd>move .+1<CR>==gi<ESC>')
+-- kset('n', '<A-k>', '<cmd>move .-2<CR>==gi<ESC>')
+kset('v', '<A-j>', '<cmd>move .+1<CR>==')
+kset('v', '<A-k>', '<cmd>move .-2<CR>==')
+-- kset('x', '<A-j>', "<cmd>move '>+1<CR>gv-gv")
+-- kset('x', '<A-k>', "<cmd>move '<-2<CR>gv-gv")
 
 -- Stay in indent mode
 kset('v', '<', '<gv')
 kset('v', '>', '>gv')
 
 -- Spell check
-kset('n', '<leader>o', ':setlocal spell! spelllang=da,en<CR>')
+kset('n', '<leader>o', '<cmd>setlocal spell! spelllang=da,en<CR>')
 
 -- Pandoc compilation
-kset('n', '<leader>xpdf', ':!pandoc -f markdown -t pdf %<CR><CR>')
-kset('n', '<leader>xepub', ':!pandoc -f markdown -t epub %<CR><CR>')
+kset('n', '<leader>xpdf', '<cmd>!pandoc -f markdown -t pdf %<CR><CR>')
+kset('n', '<leader>xepub', '<cmd>!pandoc -f markdown -t epub %<CR><CR>')
 
 -- Git
-kset('n', '<leader>gs', ':Gitsigns toggle_signs<CR>')
-kset('n', '<leader>gg', ':Git<CR>')
-kset('n', '<leader>gc', ':Gcommit<space>%<CR>')
-kset('n', '<leader>gw', ':Gwrite<CR>')
+kset('n', '<leader>gs', '<cmd>Gitsigns toggle_signs<CR>')
+kset('n', '<leader>gg', '<cmd>Git<CR>')
+kset('n', '<leader>gc', '<cmd>Gcommit<space>%<CR>')
+kset('n', '<leader>gw', '<cmd>Gwrite<CR>')
 
 -- Other plugins
-kset('n', '<leader>go', ':Goyo<CR>')
-kset('n', '<leader>e', ':NvimTreeToggle<CR>')
-kset('n', '<leader>mp', ':MarkdownPreviewToggle<CR>')
-kset('n', '<leader>t', ':Telescope find_files<CR>')
-kset('n', '<leader>f', ':lua vim.lsp.buf.formatting_sync()<CR>')
+kset('n', '<leader>go', '<cmd>Goyo<CR>')
+kset('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
+kset('n', '<leader>mp', '<cmd>MarkdownPreviewToggle<CR>')
+kset('n', '<leader>t', '<cmd>Telescope find_files<CR>')
+kset('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>')
