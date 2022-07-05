5a6a771c62a78a519b35c6b0ae301b1311e205ae
Noah Elkhatali <noahpc@live.dk>
2022-07-05 03:00:31 +0200

    added keymap for cycling through snippet node choices

--- nvim/.config/nvim/lua/user/snippets/config.lua
+++ nvim/.config/nvim/lua/user/snippets/config.lua
@@ -1,8 +1,7 @@
-
 local ls = require 'luasnip'
 
 ls.config.set_config {
-    history = true,
+    history = true, -- allows jumping back into most recent snippet
     updateevents = 'TextChanged,TextChangedI',
 }
 
@@ -15,5 +14,11 @@ end, { silent = true })
 vim.keymap.set({ 'i', 's' }, '<C-j>', function()
     if ls.jumpable(-1) then
         ls.jump(-1)
+    end
+end, { silent = true })
+
+vim.keymap.set('i', '<C-l>', function()
+    if ls.choice_active() then
+        ls.change_choice(1)
     end
 end, { silent = true })
