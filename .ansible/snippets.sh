4b170f1042481d53c119d43e549268b7c1f1035a
Nuhddy <noahpc@live.dk>
2023-03-11 02:28:40 +0100

    added ansible stuff - wip

---
+++ .ansible/snippets.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+update_pacman_pkglist() {
+    echo "pacman_pkgs:" > pacman_pkgs.yaml
+    pacman -Qen | awk '{ print "- "$1 }' >> pacman_pkgs.yaml
+}
+
+update_aur_pkglist() {
+    echo "aur_pkgs:" > aur_pkgs.yaml
+    pacman -Qem | awk '{ print "- "$1 }' >> aur_pkgs.yaml
+}
