8598cf93ae35c6004e1c234c320c5ebc553b71b4
Nuhddy <noahpc@live.dk>
2025-03-12 20:01:00 +0000

    add wsl utilities

--- hosts/fennec/home.nix
+++ hosts/fennec/home.nix
@@ -1,8 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ../../home-manager-modules/core
     ../../home-manager-modules/common
     ../../home-manager-modules/neovim
+  ];
+
+  home.packages = with pkgs; [
+    wslu
   ];
 
   home.stateVersion = "24.11";
