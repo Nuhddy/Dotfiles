2d5a523ae752040e929307f995f20b4e8f230dc1
Nuhddy <noahpc@live.dk>
2025-03-28 21:20:36 +0000

    fix variable vault path

--- configs/nvim/lua/plugins/obsidian.lua
+++ configs/nvim/lua/plugins/obsidian.lua
@@ -1,3 +1,21 @@
+local function obs_path(sub_dir)
+    local root_dir
+
+    if _G.OS == 'WSL' then
+        root_dir = '/mnt/c/Users/'
+            .. vim.loop.os_getenv 'USER' ---@diagnostic disable-line
+            .. '/Nextcloud/Obsidian/'
+    elseif _G.OS == 'Windows' then
+        root_dir = 'C:\\Users\\'
+            .. vim.loop.os_getenv 'USER' ---@diagnostic disable-line
+            .. '\\Nextcloud\\Obsidian\\'
+    else
+        root_dir = '~/Nextcloud/Documents/Obsidian/'
+    end
+
+    return root_dir .. sub_dir
+end
+
 return {
     'epwalsh/obsidian.nvim',
     version = '*',
@@ -8,7 +26,7 @@ return {
         workspaces = {
             {
                 name = 'Workouts',
-                path = '~/Nextcloud/Documents/Obsidian/Workouts',
+                path = obs_path 'Workouts',
                 overrides = {
                     disable_frontmatter = true,
                     templates = {
