5a77abf676e960a899d9ccdd645d7d9526ce9120
Nuhddy <noahpc@live.dk>
2025-10-22 21:33:36 +0200

    add new host

---
+++ hosts/breakout/configuration.nix
@@ -0,0 +1,23 @@
+{
+  inputs,
+  system,
+  ...
+}: {
+  imports = [
+    inputs.nixos-wsl.nixosModules.default
+    ../../nixos-modules/core.nix
+
+    ../../nixos-modules/workstation.nix
+  ];
+
+  networking.hostName = "breakout";
+  system.stateVersion = "25.05";
+
+  nixpkgs.hostPlatform = system;
+
+  wsl = {
+    enable = true;
+    defaultUser = "nuhddy";
+    docker-desktop.enable = true;
+  };
+}
---
+++ hosts/breakout/home.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  imports = [
+    ../../home-manager-modules/core.nix
+
+    ../../home-manager-modules/workstation
+  ];
+
+  home.packages = with pkgs; [
+    wslu
+  ];
+
+  home.stateVersion = "25.05";
+}
