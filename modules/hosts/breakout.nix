4531c616703b7c052fd6530cc37b15598537966a
Nuhddy <noahpc@live.dk>
2026-01-02 21:07:42 +0100

    refactor breakout host

---
+++ modules/hosts/breakout.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  lib,
+  config,
+  ...
+}: let
+  host = "breakout";
+  user = "nuhddy";
+in {
+  flake.nixosConfigurations.${host} = (import ../../lib/mk-configuration.nix {inherit inputs lib config;}) {
+    inherit host user;
+    nixosStateVersion = "25.05";
+    homeManagerStateVersion = "25.05";
+    nixosConfig = {
+      imports = [inputs.nixos-wsl.nixosModules.default];
+      wsl = {
+        enable = true;
+        defaultUser = user;
+        docker-desktop.enable = true;
+      };
+    };
+    homeManagerConfig = {pkgs, ...}: {
+      home.packages = [pkgs.wslu];
+    };
+    aspects = [
+      "base"
+      "workstation"
+      "neovim"
+    ];
+  };
+}
