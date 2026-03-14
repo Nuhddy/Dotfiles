bfa8106ce230bb7435a218e2973b5b90f7417a5a
Nuhddy <noahpc@live.dk>
2026-03-14 19:53:58 +0100

    add nixpkgs stable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
@@ -13,7 +14,6 @@
     };
     nixos-wsl.url = "github:/nix-community/NixOS-WSL/main";
 
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     nixcord.url = "github:kaylorben/nixcord";
     catppuccin.url = "github:catppuccin/nix";
     niri.url = "github:sodiboo/niri-flake";
