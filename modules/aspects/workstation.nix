68ceb0c5c1e2d230da3af1216d1782ac67eb68c8
Nuhddy <noahpc@live.dk>
2026-05-17 01:08:06 +0200

    add binary caches

--- modules/aspects/workstation.nix
+++ modules/aspects/workstation.nix
@@ -58,19 +58,26 @@
       devenv
       dotnet-sdk
       android-tools
+      cachix
 
       # Git
       git-crypt
     ];
 
     # Cachix for devenv
-    # nix.settings = {
-    #   substituters = "https://devenv.cachix.org";
-    #   trusted-public-keys = [
-    #     "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    #     "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-    #   ];
-    # };
+    nix.settings = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://devenv.cachix.org"
+        "https://nixpkgs-python.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      ];
+    };
 
     catppuccin = {
       enable = true;
